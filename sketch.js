9393432794dc942afd4418fc59c6db4383c096b3
VedanshGupta <68778187+VedanshGupta@users.noreply.github.com>
2020-12-08 17:35:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,12 +47,12 @@ function draw() {
   strokeWeight(10);
   noFill();
 
-  stroke(255,0,0);
+  stroke(0,0,255);
   arc(0,0,300,300,0,scAngle);
 
   stroke(0,255,0);
   arc(0,0,280,280,0,mnAngle);
 
-  stroke(0,0,255);
+  stroke(255,0,0);
   arc(0,0,260,260,0,hrAngle);
 }
